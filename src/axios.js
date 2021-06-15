9e13078969a0a35f3ad7be21feb8862375e977d7
janz-emmanuel24 <jansabbas1@gmail.com>
2021-06-15 20:41:15 +0300

    back_end

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/art-ug/us-central1/api' // the api // cloud function
-  // baseURL: 'https://us-central1-art-ug.cloudfunctions.net/api'
+  // baseURL: 'http://localhost:5001/art-ug/us-central1/api' // the api // cloud function
+  baseURL: 'https://us-central1-art-ug.cloudfunctions.net/api'
 });
 
 export default instance;
